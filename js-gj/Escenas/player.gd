d87d41b7b4545bfbc2447dc59e52abc9f09009ff
Ciro <cirofontano@gmail.com>
2025-04-18 21:37:12 -0300

    ciro

--- js-gj/Escenas/player.gd
+++ js-gj/Escenas/player.gd
@@ -24,6 +24,9 @@ func _physics_process(delta: float) -> void:
 	des_vel = des_vel.normalized()*speed
 	if Input.is_key_pressed(KEY_SHIFT):
 		des_vel *= 2
+	if $Ray_suelo.is_colliding():
+		$MeshInstance3D2.global_position = $Ray_suelo.get_collision_point() + $Ray_suelo.get_collision_normal()*5
+	$MeshInstance3D2.visible = $Ray_suelo.is_colliding()
 	apply_force(Vector3((des_vel-linear_velocity).x,0,(des_vel-linear_velocity).z),Vector3.UP)
 	if des_vel == Vector3.ZERO:
 		apply_force(Vector3((des_vel-linear_velocity).x,0,(des_vel-linear_velocity).z)*0.5,Vector3.UP)
@@ -47,3 +50,4 @@ func _physics_process(delta: float) -> void:
 	camera_des_pos += (position *0.8 + des_look_point*0.2 + camera_angle + Vector3(linear_velocity.x,0,linear_velocity.z)/3 -camera_des_pos)*delta*5
 	camera_des_pos = camera_des_pos.normalized() * min(camera_des_pos.length(),get_parent().terr_size-3)
 	$Camera3D.global_position = camera_des_pos
+	$Ray_suelo.global_rotation = Vector3.ZERO
